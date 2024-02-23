0889a514302d30de4109d6e1a9186d1997402b19
Shrikrushna Ade <shrikrushnaade766@gmail.com>
2024-02-23 09:56:51 +0530

    Postfolio website

---
+++ collab/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <h1 className="text-3xl text-center text-white pt-3 ">Next Js with Avtar</h1>
+    </main>
+    </>
+  );
+}
