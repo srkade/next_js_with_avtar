9ef41c4ccc359ccabf42458913b5b6371bba254b
Shrikrushna Ade <122589277+srkade@users.noreply.github.com>
2024-07-11 05:44:04 +0000

    created ai content creater project and started with Header and side Navbar

---
+++ ai-content-creater/app/dashboard/_components/SideNav.tsx
@@ -0,0 +1,46 @@
+import { FileClock, Home, Settings, WalletCards } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
+
+function SideNav() {
+    const MenuList =[
+        {
+            name:'Home',
+            icon:Home,
+            path:'/dashboard'
+        },
+        {
+            name:'History',
+            icon:FileClock,
+            path:'/dashboard/history'
+        },
+        {
+            name:'Billing',
+            icon:WalletCards,
+            path:'/dashboard/billing'
+        },
+        {
+            name:'Setting',
+            icon:Settings,
+            path:'/dashboard/setting'
+        },
+    ]
+  return (
+    <div className='h-screen p-5 shadow-sm border'>
+        <div className='flex justify-center border'>
+        <Image src={'/logo.svg'} alt='logo' width={100} height={100} />
+        </div>
+        <hr className='my-6' />
+        <div className='mt-10'>
+            {MenuList.map((menu,index)=>{
+              return  <div className='flex gap-2 items-center mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer'>
+                    <menu.icon className='h-6 w-6'/>
+                    <h2 className='text-lg'>{menu.name}</h2>
+                </div>
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default SideNav
---
+++ ai-content-creater/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1>Subscribe To Shree Ade channel</h1>
+    <Button variant="outline" >Subscribe</Button>
+    </>
+  );
+}
